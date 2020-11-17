99c3ea9c90138762028bdda1b21a6283c2721233
jakoofficial <jacobvoandersen@gmail.com>
2020-11-17 06:34:15 +0100

    working on animation in code

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     private GameManager gm;
-
+    static float t = 0.0f;
+    private float minimum = 1.75F;
+    private float maximum = 2.0F;
     private void Start()
     {
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -15,5 +17,29 @@ public class ButtonController : MonoBehaviour
     {
         gm.points++;
         gm.UpdateText();
+    }
+
+    private void OnMouseOver()
+    {
+        
+        this.transform.localScale = new Vector3(Mathf.Lerp(minimum, maximum, t),Mathf.Lerp(minimum, maximum, t), 1f);
+        
+        // .. and increase the t interpolater
+        t += 0.7f * Time.deltaTime;
+
+        // now check if the interpolator has reached 1.0
+        // and swap maximum and minimum so game object moves
+        // in the opposite direction.
+        if (t > 1.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            t = 0.0f;
+        }
+    }
+    private void OnMouseExit()
+    {
+        this.transform.localScale = new Vector3(2, 2, 1);
     }
 }
